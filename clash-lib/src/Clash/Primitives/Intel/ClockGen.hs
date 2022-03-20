samalws/clash-compiler

commit d32555775000cb5860c5d89aa2dc5796fe89b7ed

Fold identifier logic into 'Clash.Netlist.Id'. Fix #1230
This megacommit moves all identifier (uniqueness) logic to a single
module "Clash.Netlist.Id". "Identifier" is no longer a type alias for
Text, but a data type allowing cheap new unique identifier generation.

It fixes a number of (unreported) bugs:

 * #1230: Clash no longer produces "name conflicts" between basic and
   extended identifiers. I.e., \x\ and x are now considered the same
   variable in VHDL (likewise for other HDLs). Although the VHDL spec
   considers them distinct variables, some HDL tools - like Quartus -
   don't.

 * Capitalization of Haskell names are now preserved in VHDL. Note that
   VHDL is a case insensitive languages, so there are measures in place
   to prevent Clash from generating both Foo and fOO. This used to be
   handled by promoting every capitalized identifier to an extended one,
   and wasn't handled for basic ones.

 * Names generated for testbenches can no longer cause collisions with
   previously generated entities.

 * Names generated for components can no longer cause collisions with
   user specified top entity names.

 * For (System)Verilog, variables can no longer cause collisions with
   (to be) generated entity names.

 * HO blackboxes can no longer cause collisions with identifiers
   declared in their surrounding architecture block.

clash-lib/src/Clash/Primitives/Intel/ClockGen.hs
@@ -14,15 +14,16 @@ module Clash.Primitives.Intel.ClockGen where
 
 import Clash.Backend
 import Clash.Netlist.BlackBox.Util
-import Clash.Netlist.Id
+import qualified Clash.Netlist.Id as Id
 import Clash.Netlist.Types
-import Clash.Netlist.Util hiding (mkUniqueIdentifier)
+import Clash.Netlist.Util
 
 import Control.Monad.State
 
 import Data.Semigroup.Monad
 import qualified Data.String.Interpolate.IsString as I
 import Data.Text.Prettyprint.Doc.Extra
+import TextShow (showt)
 
 import qualified Data.Text as TextS
 
@@ -69,28 +70,33 @@ alteraPllQsysTF = TemplateFunction used valid alteraPllQsysTemplate
   valid _ = False
 
 alteraPllTemplate
-  :: Backend s
+  :: forall s
+   . Backend s
   => BlackBoxContext
   -> State s Doc
 alteraPllTemplate bbCtx = do
- let mkId = mkUniqueIdentifier Basic
- locked <- mkId "locked"
- pllLock <- mkId "pllLock"
- alteraPll <- mkId "altera_pll_block"
- alteraPll_inst <- mkId instname0
+ locked <- Id.makeBasic "locked"
+ pllLock <- Id.makeBasic "pllLock"
+ alteraPll <- Id.makeBasic "altera_pll_block"
+ alteraPll_inst <- Id.makeBasic instname0
 
- clocks <- traverse (mkUniqueIdentifier Extended)
-                    [TextS.pack ("pllOut" ++ show n) | n <- [0..length tys - 1]]
+ clocks <- Id.nextN (length tys - 1) =<< Id.make "pllOut"
+
+  -- TODO: unsafeMake is dubious here: I don't think we take names in
+  -- TODO: bbQsysIncName into account when generating fresh ids
+ let compName = Id.unsafeMake (head (bbQsysIncName bbCtx))
+
+ let outclkPorts = map (\n -> toPort ("outclk_" <> showt n)) [(0 :: Int)..length clocks-1]
+
  getMon $ blockDecl alteraPll $ concat
   [[ NetDecl Nothing locked  rstTy
    , NetDecl' Nothing Reg pllLock (Right Bool) Nothing]
   ,[ NetDecl Nothing clkNm ty | (clkNm,ty) <- zip clocks tys]
   ,[ InstDecl Comp Nothing [] compName alteraPll_inst [] $ concat
-      [[(Identifier "refclk" Nothing,In,clkTy,clk)
-       ,(Identifier "rst" Nothing,In,rstTy,rst)]
-      ,[(Identifier (TextS.pack ("outclk_" ++ show n)) Nothing,Out,ty,Identifier k Nothing)
-       |(k,ty,n) <- zip3 clocks tys [(0 :: Int)..]  ]
-      ,[(Identifier "locked" Nothing,Out,rstTy,Identifier locked Nothing)]]
+      [ [ (toPort "refclk", In, clkTy, clk)
+        , (toPort "rst", In, rstTy, rst)]
+      , [ (p, Out, ty, Identifier k Nothing) | (k, ty, p) <- zip3 clocks tys outclkPorts ]
+      , [(toPort "locked", Out, rstTy, Identifier locked Nothing)]]
    , CondAssignment pllLock Bool (Identifier locked Nothing) rstTy
       [(Just (BitLit H),Literal Nothing (BoolLit True))
       ,(Nothing        ,Literal Nothing (BoolLit False))]
@@ -105,28 +111,34 @@ alteraPllTemplate bbCtx = do
   [(nm,_,_),(clk,clkTy,_),(rst,rstTy,_)] = drop 3 (bbInputs bbCtx)
   Just nm' = exprToString nm
   instname0 = TextS.pack nm'
-  compName = head (bbQsysIncName bbCtx)
+
+  -- unsafeMake is safe here: it's just port names predefined by the PLL IP
+  toPort p = Identifier (Id.unsafeMake p) Nothing
 
 altpllTemplate
   :: Backend s
   => BlackBoxContext
   -> State s Doc
 altpllTemplate bbCtx = do
- let mkId = mkUniqueIdentifier Basic
- pllOut <- mkId "pllOut"
- locked <- mkId "locked"
- pllLock <- mkId "pllLock"
- alteraPll <- mkId "altpll_block"
- alteraPll_inst <- mkId instname0
+ pllOut <- Id.make "pllOut"
+ locked <- Id.make "locked"
+ pllLock <- Id.make "pllLock"
+ alteraPll <- Id.make "altpll_block"
+ alteraPll_inst <- Id.make instname0
+
+ -- TODO: unsafeMake is dubious here: I don't think we take names in
+ -- TODO: bbQsysIncName into account when generating fresh ids
+ let compName = Id.unsafeMake (head (bbQsysIncName bbCtx))
+
  getMon $ blockDecl alteraPll
   [ NetDecl Nothing locked  Bit
   , NetDecl' Nothing Reg pllLock (Right Bool) Nothing
   , NetDecl Nothing pllOut clkOutTy
   , InstDecl Comp Nothing [] compName alteraPll_inst []
-      [(Identifier "clk" Nothing,In,clkTy,clk)
-      ,(Identifier "areset" Nothing,In,rstTy,rst)
-      ,(Identifier "c0" Nothing,Out,clkOutTy,Identifier pllOut Nothing)
-      ,(Identifier "locked" Nothing,Out,Bit,Identifier locked Nothing)]
+      [ (toPort "clk", In, clkTy, clk)
+      , (toPort "areset", In, rstTy, rst)
+      , (toPort "c0", Out, clkOutTy, Identifier pllOut Nothing)
+      , (toPort "locked", Out, Bit, Identifier locked Nothing)]
   , CondAssignment pllLock Bool (Identifier locked Nothing) rstTy
       [(Just (BitLit H),Literal Nothing (BoolLit True))
       ,(Nothing        ,Literal Nothing (BoolLit False))]
@@ -140,7 +152,9 @@ altpllTemplate bbCtx = do
   [(Identifier result Nothing,resTy@(Product _ _ [clkOutTy,_]))] = bbResults bbCtx
   Just nm' = exprToString nm
   instname0 = TextS.pack nm'
-  compName = head (bbQsysIncName bbCtx)
+
+  -- unsafeMake is safe here: it's just port names predefined by the PLL IP
+  toPort p = Identifier (Id.unsafeMake p) Nothing
 
 
 altpllQsysTemplate

benchmark/common/BenchmarkCommon.hs
@@ -5,6 +5,7 @@
 
 module BenchmarkCommon where
 
+import Clash.Annotations.Primitive (HDL(VHDL))
 import Clash.Annotations.BitRepresentation.Internal (CustomReprs, buildCustomReprs)
 import Clash.Backend
 import Clash.Backend.VHDL
@@ -56,6 +57,9 @@ opts idirs =
     , opt_importPaths=idirs
     , opt_specLimit=40 -- For "PipelinesViaFolds"
     }
+
+hdl :: HDL
+hdl = VHDL
 
 backend :: VHDLState
 backend = initBackend WORD_SIZE_IN_BITS HDLSYN True Nothing (AggressiveXOptBB False)

benchmark/profiling/run/profile-netlist-run.hs
@@ -3,14 +3,13 @@ import           Clash.Backend
 import           Clash.Core.Name
 import           Clash.Core.TyCon
 import           Clash.Core.Var
+import           Clash.Core.VarEnv (mkVarEnv)
 import           Clash.Driver.Types
 import           Clash.Netlist
 import           Clash.Netlist.Types          hiding (backend, hdlDir)
 
 import           Control.DeepSeq              (deepseq)
-import           Control.Monad.State          (evalState)
 import           Data.Binary                  (decode)
-import qualified Data.HashMap.Strict          as HashMap
 import           Data.List                    (partition)
 import           Data.Maybe                   (fromMaybe)
 import qualified Data.Text                    as Text
@@ -44,17 +43,16 @@ benchFile idirs src = do
       topEntityS = Text.unpack (nameOcc (varName topEntity))
       modName    = takeWhile (/= '.') topEntityS
       hdlState'  = setModName (Text.pack modName) backend
-      mkId1      = evalState mkIdentifier hdlState'
-      extId      = evalState extendIdentifier hdlState'
-      prefixM    = ComponentPrefix Nothing Nothing
+      (compNames, seen) = genTopNames Nothing True hdl topEntities
+      topEntityMap = mkVarEnv (zip (map topId topEntities) topEntities)
+      prefixM    = Nothing
       ite        = ifThenElseExpr hdlState'
-      seen       = HashMap.empty
       hdlDir     = fromMaybe "." (opt_hdlDir opts1) </>
                          Clash.Backend.name hdlState' </>
                          takeWhile (/= '.') topEntityS
-  (netlist,_) <-
-    genNetlist False opts1 reprs transformedBindings topEntities primMap'
-               tcm typeTrans iw mkId1 extId ite (SomeBackend hdlState') seen hdlDir prefixM topEntity
+  (netlist,_,_) <-
+    genNetlist False opts1 reprs transformedBindings topEntityMap compNames primMap'
+               tcm typeTrans iw ite (SomeBackend hdlState') seen hdlDir prefixM topEntity
   netlist `deepseq` putStrLn ".. done\n"
 
 setupEnv

clash-cores/src/Clash/Cores/LatticeSemi/ECP5/Blackboxes/IO.hs
@@ -13,7 +13,7 @@ module Clash.Cores.LatticeSemi.ECP5.Blackboxes.IO (bbTF) where
 
 import           Clash.Backend
 import           Clash.Netlist.BlackBox.Util
-import           Clash.Netlist.Id
+import qualified Clash.Netlist.Id                as Id
 import           Clash.Netlist.Types
 import           Control.Monad.State             (State())
 import           Data.Semigroup.Monad            (getMon)
@@ -35,11 +35,11 @@ bbTemplate
   => BlackBoxContext
   -> State s Doc
 bbTemplate bbCtx = do
-  let mkId = mkUniqueIdentifier Basic
+  bb      <- Id.makeBasic "bb"
+  bb_inst <- Id.makeBasic "bb_inst"
+  dIn     <- Id.makeBasic "dIn"
 
-  bb      <- mkId "bb"
-  bb_inst <- mkId "bb_inst"
-  dIn     <- mkId "dIn"
+  compName <- Id.addRaw (TextS.pack compName')
 
   getMon $ blockDecl bb $
     [ NetDecl Nothing dIn Bit
@@ -48,10 +48,10 @@ bbTemplate bbCtx = do
       ]
       [ -- NOTE: Direction is set to 'In', but will be rendered as inout due to
         -- its type packagePinTy
-        (Identifier "B" Nothing, In, packagePinTy, packagePin)
-      , (Identifier "T" Nothing, In,  Bool, outputEnable)
-      , (Identifier "I" Nothing, In,  Bit, dOut)
-      , (Identifier "O" Nothing, Out, Bit, Identifier dIn Nothing)
+        (instPort "B", In, packagePinTy, packagePin)
+      , (instPort "T", In,  Bool, outputEnable)
+      , (instPort "I", In,  Bit, dOut)
+      , (instPort "O", Out, Bit, Identifier dIn Nothing)
       ]
     , Assignment result (Identifier dIn Nothing)
     ]
@@ -66,6 +66,6 @@ bbTemplate bbCtx = do
    ] = bbInputs bbCtx
 
   Just compName' = exprToString intrinsicName
-  compName = TextS.pack compName'
+  instPort pn = Identifier (Id.unsafeMake pn) Nothing
 
   [(Identifier result Nothing,_)] = bbResults bbCtx

clash-cores/src/Clash/Cores/LatticeSemi/ICE40/Blackboxes/IO.hs
@@ -19,7 +19,7 @@ import           GHC.Stack
   (HasCallStack, prettyCallStack, callStack)
 
 import           Clash.Backend
-import           Clash.Netlist.Id
+import qualified Clash.Netlist.Id as Id
 import           Clash.Netlist.Types
 
 pinConfigLiteral
@@ -51,14 +51,15 @@ sbioTemplate
   => BlackBoxContext
   -> State s Doc
 sbioTemplate bbCtx = do
-  let mkId = mkUniqueIdentifier Basic
-      compName = "SB_IO"  -- Hardcoded for now
+  let compName = Id.unsafeMake "SB_IO"  -- Hardcoded for now
 
-  sbio <- mkId "sbio"
-  sbio_inst <- mkId "sbio_inst"
+  sbio <- Id.makeBasic "sbio"
+  sbio_inst <- Id.makeBasic "sbio_inst"
 
-  dIn0 <- mkId "dIn0"
-  dIn1 <- mkId "dIn1"
+  dIn0 <- Id.makeBasic "dIn0"
+  dIn1 <- Id.makeBasic "dIn1"
+
+  let u = Id.unsafeMake
 
   let
     resultTuple =
@@ -73,22 +74,22 @@ sbioTemplate bbCtx = do
     [ NetDecl Nothing dIn0 Bit
     , NetDecl Nothing dIn1 Bit
     , InstDecl Comp Nothing [] compName sbio_inst
-      [ (Identifier "PIN_TYPE" Nothing, BitVector 6, pinConfig)
+      [ (Identifier (u "PIN_TYPE") Nothing, BitVector 6, pinConfig)
       ]
       [ -- NOTE: Direction is set to 'In', but will be rendered as inout due to
         -- its the type packackagePinTy
-        (Identifier "PACKAGE_PIN" Nothing, In, packagePinTy, packagePin)
-      , (Identifier "LATCH_INPUT_VALUE" Nothing, In, Bit, latchInput)
+        (Identifier (u "PACKAGE_PIN") Nothing, In, packagePinTy, packagePin)
+      , (Identifier (u "LATCH_INPUT_VALUE") Nothing, In, Bit, latchInput)
       -- TODO: If clock is constantly enabled, docs recommend  not connecting
       -- TODO: CLOCK_ENABLE at all.
-      , (Identifier "CLOCK_ENABLE" Nothing, In, Bool, en)
-      , (Identifier "INPUT_CLK" Nothing, In, clkTy, clk)
-      , (Identifier "OUTPUT_CLK" Nothing, In, clkTy, clk)
-      , (Identifier "OUTPUT_ENABLE" Nothing, In, Bool, outputEnable)
-      , (Identifier "D_OUT_0" Nothing, In, Bit, dOut0)
-      , (Identifier "D_OUT_1" Nothing, In, Bit, dOut1)
-      , (Identifier "D_IN_0" Nothing, Out, Bit, Identifier dIn0 Nothing)
-      , (Identifier "D_IN_1" Nothing, Out, Bit, Identifier dIn1 Nothing)
+      , (Identifier (u "CLOCK_ENABLE") Nothing, In, Bool, en)
+      , (Identifier (u "INPUT_CLK") Nothing, In, clkTy, clk)
+      , (Identifier (u "OUTPUT_CLK") Nothing, In, clkTy, clk)
+      , (Identifier (u "OUTPUT_ENABLE") Nothing, In, Bool, outputEnable)
+      , (Identifier (u "D_OUT_0") Nothing, In, Bit, dOut0)
+      , (Identifier (u "D_OUT_1") Nothing, In, Bit, dOut1)
+      , (Identifier (u "D_IN_0") Nothing, Out, Bit, Identifier dIn0 Nothing)
+      , (Identifier (u "D_IN_1") Nothing, Out, Bit, Identifier dIn1 Nothing)
       ]
     , Assignment result resultTuple
     ]

clash-ghc/src-ghc/Clash/GHC/ClashFlags.hs
@@ -24,6 +24,7 @@ import           Data.IORef
 import           Data.List                      (dropWhileEnd)
 import           Data.List.Split                (splitOn)
 import qualified Data.Set                       as Set
+import qualified Data.Text                      as Text
 import           Text.Read                      (readMaybe)
 
 import           Clash.Driver.Types
@@ -215,7 +216,8 @@ setComponentPrefix
   :: IORef ClashOpts
   -> String
   -> IO ()
-setComponentPrefix r s = modifyIORef r (\c -> c {opt_componentPrefix = Just s})
+setComponentPrefix r s =
+  modifyIORef r (\c -> c {opt_componentPrefix = Just (Text.pack s)})
 
 setOldInlineStrategy :: IORef ClashOpts -> IO ()
 setOldInlineStrategy r = modifyIORef r (\c -> c {opt_newInlineStrat = False})

clash-lib/clash-lib.cabal
@@ -234,6 +234,10 @@ Library
                       Clash.Netlist.BlackBox.Types
                       Clash.Netlist.BlackBox.Util
                       Clash.Netlist.Id
+                      Clash.Netlist.Id.Common
+                      Clash.Netlist.Id.SystemVerilog
+                      Clash.Netlist.Id.Verilog
+                      Clash.Netlist.Id.VHDL
                       Clash.Netlist.Types
                       Clash.Netlist.Util
 
@@ -271,6 +275,7 @@ Library
                       Data.Text.Prettyprint.Doc.Extra
 
   Other-Modules:      Clash.Annotations.TopEntity.Extra
+                      Control.Applicative.Extra
                       Data.Aeson.Extra
                       Data.List.Extra
                       Data.Primitive.ByteArray.Extra
@@ -294,7 +299,7 @@ test-suite unittests
   type:             exitcode-stdio-1.0
   default-language: Haskell2010
   main-is:          unittests.hs
-  ghc-options:      -Wall -Wcompat
+  ghc-options:      -Wall -Wcompat -threaded -with-rtsopts=-N
   hs-source-dirs:   tests
 
   if !flag(unittests)
@@ -307,6 +312,7 @@ test-suite unittests
       ghc-typelits-knownnat,
 
       base,
+      bytestring,
       containers,
       concurrent-supply,
       data-default,
@@ -314,8 +320,10 @@ test-suite unittests
       haskell-src-exts,
       ghc,
       lens,
+      quickcheck-text,
       tasty         >= 1.2      && < 1.4,
       tasty-hunit,
+      tasty-quickcheck,
       template-haskell,
       text,
       transformers,
@@ -323,6 +331,7 @@ test-suite unittests
 
   Other-Modules: Clash.Tests.Core.FreeVars
                  Clash.Tests.Core.Subst
+                 Clash.Tests.Netlist.Id
                  Clash.Tests.Util.Interpolate
                  Clash.Tests.Normalize.Transformations
 

clash-lib/src/Clash/Backend.hs
@@ -10,14 +10,9 @@
 
 module Clash.Backend where
 
-import Control.Lens                         (Lens')
 import qualified  Control.Lens              as Lens
-import Data.HashMap.Strict                  (HashMap)
-import qualified Data.HashMap.Strict        as HashMap
 import Data.HashSet                         (HashSet)
-import Data.Maybe                           (fromMaybe)
 import Data.Semigroup.Monad                 (Mon (..))
-import qualified Data.Text                  as T
 import Data.Text                            (Text)
 import qualified Data.Text.Lazy             as LT
 import Control.Monad.State                  (State)
@@ -25,7 +20,6 @@ import Data.Text.Prettyprint.Doc.Extra      (Doc)
 
 import SrcLoc (SrcSpan)
 
-import Clash.Netlist.Id
 import {-# SOURCE #-} Clash.Netlist.Types
 import Clash.Netlist.BlackBox.Types
 
@@ -52,7 +46,7 @@ clashVer = Data.Version.showVersion Paths_clash_lib.version
 clashVer = "development"
 #endif
 
-type ModName = Identifier
+type ModName = Text
 
 -- | Is a type used for internal or external use
 data Usage
@@ -64,7 +58,7 @@ data Usage
 -- | Is '-fclash-aggresive-x-optimization-blackbox' set?
 newtype AggressiveXOptBB = AggressiveXOptBB Bool
 
-class Backend state where
+class HasIdentifierSet state => Backend state where
   -- | Initial state for state monad
   initBackend
     :: Int
@@ -91,9 +85,9 @@ class Backend state where
   extractTypes     :: state -> HashSet HWType
 
   -- | Generate HDL for a Netlist component
-  genHDL           :: Identifier -> SrcSpan -> HashMap Identifier Word -> Component -> Mon (State state) ((String, Doc),[(String,Doc)])
+  genHDL           :: ModName -> SrcSpan -> IdentifierSet -> Component -> Mon (State state) ((String, Doc),[(String,Doc)])
   -- | Generate a HDL package containing type definitions for the given HWTypes
-  mkTyPackage      :: Identifier -> [HWType] -> Mon (State state) [(String, Doc)]
+  mkTyPackage      :: ModName -> [HWType] -> Mon (State state) [(String, Doc)]
   -- | Convert a Netlist HWType to a target HDL type
   hdlType          :: Usage -> HWType -> Mon (State state) Doc
   -- | Convert a Netlist HWType to an HDL error value for that type
@@ -120,10 +114,6 @@ class Backend state where
   fromBV           :: HWType -> LT.Text -> Mon (State state) Doc
   -- | Synthesis tool we're generating HDL for
   hdlSyn           :: State state HdlSyn
-  -- | mkIdentifier
-  mkIdentifier     :: State state (IdType -> Identifier -> Identifier)
-  -- | mkIdentifier
-  extendIdentifier :: State state (IdType -> Identifier -> Identifier -> Identifier)
   -- | setModName
   setModName       :: ModName -> state -> state
   -- | setSrcSpan
@@ -131,9 +121,7 @@ class Backend state where
   -- | getSrcSpan
   getSrcSpan       :: State state SrcSpan
   -- | Block of declarations
-  blockDecl        :: Text -> [Declaration] -> Mon (State state) Doc
-  -- | unextend/unescape identifier
-  unextend         :: State state (Identifier -> Identifier)
+  blockDecl        :: Identifier -> [Declaration] -> Mon (State state) Doc
   addIncludes      :: [(String, Doc)] -> State state ()
   addLibraries     :: [LT.Text] -> State state ()
   addImports       :: [LT.Text] -> State state ()
@@ -141,54 +129,16 @@ class Backend state where
   getDataFiles     :: State state [(String,FilePath)]
   addMemoryDataFile  :: (String,String) -> State state ()
   getMemoryDataFiles :: State state [(String,String)]
-  seenIdentifiers  :: Lens' state (HashMap Identifier Word)
   ifThenElseExpr :: state -> Bool
   -- | Whether -fclash-aggressive-x-optimization-blackboxes was set
   aggressiveXOptBB :: State state AggressiveXOptBB
-
--- | Replace a normal HDL template placeholder with an unescaped/unextended
--- template placeholder.
---
--- Needed when the the place-holder is filled with an escaped/extended identifier
--- inside an escaped/extended identifier and we want to strip the escape
--- /extension markers. Otherwise we end up with illegal identifiers.
-escapeTemplate :: Identifier -> Identifier
-escapeTemplate "~RESULT" = "~ERESULT"
-escapeTemplate t = fromMaybe t $ do
-  t1 <- T.stripPrefix "~ARG[" t
-  n  <- T.stripSuffix "]" t1
-  pure (T.concat ["~EARG[",n,"]"])
-
-mkUniqueIdentifier
-  :: Backend s
-  => IdType
-  -> Identifier
-  -> State s Identifier
-mkUniqueIdentifier typ nm = do
-  mkId     <- mkIdentifier
-  extendId <- extendIdentifier
-  seen     <- Lens.use seenIdentifiers
-  let i = mkId typ nm
-  case HashMap.lookup i seen of
-    Just n -> go extendId n seen i
-    Nothing -> do
-     seenIdentifiers Lens.%= (HashMap.insert i 0)
-     return i
- where
-  go extendId n seen i = do
-    let i' = extendId typ i (T.pack ('_':show n))
-    case HashMap.lookup i' seen of
-       Just _ -> go extendId (n+1) seen i
-       Nothing -> do
-        seenIdentifiers Lens.%= (HashMap.insert i' (n+1))
-        return i'
 
 preserveSeen
   :: Backend s
   => Mon (State s) a
   -> Mon (State s) a
 preserveSeen m = do
-  s <- Mon (Lens.use seenIdentifiers)
+  s <- Mon (Lens.use identifierSet)
   a <- m
-  Mon (seenIdentifiers Lens..= s)
+  Mon (identifierSet Lens..= s)
   return a

commit d6d9bc638b8c763d3e82d1cef107c445a15d83d7

Fix `alteraPll` primitive
Commit d325557750 introduced an off-by-one error in the number of clocks
to generate.

Additionally, the internal `locked` signal for `alteraPll` is changed to
be of type `Bit` like in `altpll`. The borrowing of the type of the
`Reset` input probably came about out of convenience back when resets
were still a simple type so it did not matter. Now it looked odd but
worked fine nonetheless.

Also adjusted the type in `CondAssignment` in `altpll`, which showed the
same pattern.

clash-lib/src/Clash/Primitives/Intel/ClockGen.hs
@@ -80,7 +80,7 @@ alteraPllTemplate bbCtx = do
  alteraPll <- Id.makeBasic "altera_pll_block"
  alteraPll_inst <- Id.makeBasic instname0
 
- clocks <- Id.nextN (length tys - 1) =<< Id.make "pllOut"
+ clocks <- Id.nextN (length tys) =<< Id.make "pllOut"
 
   -- TODO: unsafeMake is dubious here: I don't think we take names in
   -- TODO: bbQsysIncName into account when generating fresh ids
@@ -89,15 +89,15 @@ alteraPllTemplate bbCtx = do
  let outclkPorts = map (\n -> instPort ("outclk_" <> showt n)) [(0 :: Int)..length clocks-1]
 
  getAp $ blockDecl alteraPll $ concat
-  [[ NetDecl Nothing locked  rstTy
+  [[ NetDecl Nothing locked Bit
    , NetDecl' Nothing Reg pllLock (Right Bool) Nothing]
   ,[ NetDecl Nothing clkNm ty | (clkNm,ty) <- zip clocks tys]
   ,[ InstDecl Comp Nothing [] compName alteraPll_inst [] $ NamedPortMap $ concat
       [ [ (instPort "refclk", In, clkTy, clk)
         , (instPort "rst", In, rstTy, rst)]
       , [ (p, Out, ty, Identifier k Nothing) | (k, ty, p) <- zip3 clocks tys outclkPorts ]
-      , [(instPort "locked", Out, rstTy, Identifier locked Nothing)]]
-   , CondAssignment pllLock Bool (Identifier locked Nothing) rstTy
+      , [(instPort "locked", Out, Bit, Identifier locked Nothing)]]
+   , CondAssignment pllLock Bool (Identifier locked Nothing) Bit
       [(Just (BitLit H),Literal Nothing (BoolLit True))
       ,(Nothing        ,Literal Nothing (BoolLit False))]
    , Assignment result (DataCon resTy (DC (resTy,0)) $ concat
@@ -136,7 +136,7 @@ altpllTemplate bbCtx = do
       , (instPort "areset", In, rstTy, rst)
       , (instPort "c0", Out, clkOutTy, Identifier pllOut Nothing)
       , (instPort "locked", Out, Bit, Identifier locked Nothing)]
-  , CondAssignment pllLock Bool (Identifier locked Nothing) rstTy
+  , CondAssignment pllLock Bool (Identifier locked Nothing) Bit
       [(Just (BitLit H),Literal Nothing (BoolLit True))
       ,(Nothing        ,Literal Nothing (BoolLit False))]
   , Assignment result (DataCon resTy (DC (resTy,0))